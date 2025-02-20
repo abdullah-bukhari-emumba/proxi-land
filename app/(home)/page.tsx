74902d2b47098d7e97209ba5add0a9b25d200fdc
Abdullah Bukhari <abdullah.bukhari@emumba.com>
2025-02-20 16:19:18 +0500

    hero and header injection into main page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,13 @@
 import { Container } from "@/components/layout/container";
 import PlatformCarousel from "./_components/platform-carousel";
+import Header from "./_components/header";
+import Hero from "./_components/hero";
 
 export default function Home() {
   return (
     <Container>
+      <Header/>
+      <Hero/>
       <PlatformCarousel />
     </Container>
   );
